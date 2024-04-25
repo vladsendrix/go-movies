378cce8775bbd48a5223e92f2b0c4d4ae06a4b89
vladsendrix <vladsendrix@gmail.com>
2024-04-25 22:58:19 +0300

    add some demo data

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/vladsendrix/go-movies/database"
+	"github.com/vladsendrix/go-movies/repository"
 )
 
 func main() {
@@ -13,4 +15,39 @@ func main() {
 	}
 	defer db.Close()
 
+	directorRepo := repository.NewDirectorRepository(db)
+
+	director := repository.Director{
+		Name: "Christopher Nolan",
+	}
+
+	_, err = directorRepo.Create(context.Background(), director)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	movieRepo := repository.NewMovieRepository(db)
+
+	movie := repository.Movie{
+		Title:       "Inception",
+		ReleaseYear: 2010,
+		DirectorID:  1,
+	}
+
+	_, err = movieRepo.Create(context.Background(), movie)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Director and movie added successfully")
+
+	movies, err := movieRepo.GetAll(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, m := range movies {
+		movie := m.(repository.Movie)
+		log.Printf("Movie: %s, Release Year: %d, Director ID: %d\n", movie.Title, movie.ReleaseYear, movie.DirectorID)
+	}
 }
