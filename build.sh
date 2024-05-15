77fb6e43b0bc4fd42686f3ba89b784cf833d0f0e
vladsendrix <vladsendrix@gmail.com>
2024-05-16 00:22:58 +0300

    build.sh: version binaries using the current git tag

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# Get the current Git tag
+GIT_TAG=$(git describe --tags --abbrev=0)
+
+if [ -z "$GIT_TAG" ]; then
+    echo "Error: No Git tag found."
+    exit 1
+fi
+
 # Create the bin directory if it doesn't exist
 mkdir -p bin
 
 # Build for Linux
-GOOS=linux GOARCH=amd64 go build -o bin/go-movies main.go
+GOOS=linux GOARCH=amd64 go build -ldflags="-X main.version=$GIT_TAG" -o bin/go-movies-$GIT_TAG-linux-amd64 main.go
 
 # Build for Windows
-GOOS=windows GOARCH=amd64 go build -o bin/go-movies.exe main.go
+GOOS=windows GOARCH=amd64 go build -ldflags="-X main.version=$GIT_TAG" -o bin/go-movies-$GIT_TAG-windows-amd64.exe main.go
 
 echo "Build complete. The executables are in the bin directory."
