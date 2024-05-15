8a8c84904e37990349509729aa242834007626ce
vladsendrix <vladsendrix@gmail.com>
2024-05-15 14:35:33 +0300

    add concurrency example in go code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"sync"
 
 	"github.com/vladsendrix/go-movies/database"
 	"github.com/vladsendrix/go-movies/repository"
@@ -28,6 +29,11 @@ func main() {
 
 	movieRepo := repository.NewMovieRepository(db)
 
+	err = movieRepo.DeleteAll(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	movie := repository.Movie{
 		Title:       "Inception",
 		ReleaseYear: 2010,
@@ -50,4 +56,48 @@ func main() {
 		movie := m.(repository.Movie)
 		log.Printf("Movie: %s, Release Year: %d, Director ID: %d\n", movie.Title, movie.ReleaseYear, movie.DirectorID)
 	}
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	// Transaction 1: Update a movie's release year
+	go func() {
+		defer wg.Done()
+
+		tx, err := db.Begin()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = tx.Exec("UPDATE movies SET release_year = 2022 WHERE director_id = 1")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Don't commit the transaction yet
+	}()
+
+	// Transaction 2: Read the movie's release year
+	go func() {
+		defer wg.Done()
+
+		tx, err := db.Begin()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var releaseYear int
+		err = tx.QueryRow("SELECT release_year FROM movies WHERE director_id = 1").Scan(&releaseYear)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("Release Year:", releaseYear)
+
+		if err := tx.Commit(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	wg.Wait()
 }
