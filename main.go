1a47d59eff2f3a28e088c42533b65dcaeb9eb589
vladsendrix <vladsendrix@gmail.com>
2024-05-15 16:24:46 +0300

    write concurrency issues in golang

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"context"
 	"log"
-	"sync"
 
+	"github.com/vladsendrix/go-movies/concurrency"
+	"github.com/vladsendrix/go-movies/controller"
 	"github.com/vladsendrix/go-movies/database"
-	"github.com/vladsendrix/go-movies/repository"
 	"github.com/vladsendrix/go-movies/gui"
+	"github.com/vladsendrix/go-movies/repository"
 )
 
 func main() {
@@ -17,90 +17,12 @@ func main() {
 	}
 	defer db.Close()
 
-	gui.StartGUI()
-
-	directorRepo := repository.NewDirectorRepository(db)
-
-	director := repository.Director{
-		Name: "Christopher Nolan",
-	}
-
-	_, err = directorRepo.Create(context.Background(), director)
-	if err != nil {
-		log.Fatal(err)
-	}
+	concurrency.TestConcurrency(db)
 
 	movieRepo := repository.NewMovieRepository(db)
 
-	err = movieRepo.DeleteAll(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
+	movieController := controller.NewMovieController(movieRepo)
 
-	movie := repository.Movie{
-		Title:       "Inception",
-		ReleaseYear: 2010,
-		DirectorID:  1,
-	}
+	gui.StartGUI(movieController)
 
-	_, err = movieRepo.Create(context.Background(), movie)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Director and movie added successfully")
-
-	movies, err := movieRepo.GetAll(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, m := range movies {
-		movie := m.(repository.Movie)
-		log.Printf("Movie: %s, Release Year: %d, Director ID: %d\n", movie.Title, movie.ReleaseYear, movie.DirectorID)
-	}
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	// Transaction 1: Update a movie's release year
-	go func() {
-		defer wg.Done()
-
-		tx, err := db.Begin()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		_, err = tx.Exec("UPDATE movies SET release_year = 2022 WHERE director_id = 1")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Don't commit the transaction yet
-	}()
-
-	// Transaction 2: Read the movie's release year
-	go func() {
-		defer wg.Done()
-
-		tx, err := db.Begin()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var releaseYear int
-		err = tx.QueryRow("SELECT release_year FROM movies WHERE director_id = 1").Scan(&releaseYear)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		log.Println("Release Year:", releaseYear)
-
-		if err := tx.Commit(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	wg.Wait()
 }
