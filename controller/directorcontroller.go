14c034e6a4673156bbff687c1605ed6b35d68ea5
vladsendrix <vladsendrix@gmail.com>
2024-05-15 23:18:12 +0300

    write repository and controller for director entity

---
+++ controller/directorcontroller.go
@@ -0,0 +1,72 @@
+package controller
+
+import (
+	"fmt"
+
+	"github.com/vladsendrix/go-movies/entities"
+	"github.com/vladsendrix/go-movies/repository"
+)
+
+type DirectorController struct {
+	DirectorRepo *repository.DirectorRepository
+}
+
+func NewDirectorController(directorRepo *repository.DirectorRepository) *DirectorController {
+	return &DirectorController{DirectorRepo: directorRepo}
+}
+
+func (c *DirectorController) GetByID(id int) (*entities.Director, error) {
+	movie, err := c.DirectorRepo.GetByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	directorEntity, ok := movie.(*entities.Director)
+	if !ok {
+		return nil, fmt.Errorf("could not assert type: expected *entities.Director")
+	}
+
+	return directorEntity, nil
+}
+
+func (c *DirectorController) GetAll() ([]*entities.Director, error) {
+	directors, err := c.DirectorRepo.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	directorEntities := make([]*entities.Director, len(directors))
+	for i, movie := range directors {
+		directorEntity, ok := movie.(*entities.Director)
+		if !ok {
+			return nil, fmt.Errorf("could not assert type: expected *entities.Director")
+		}
+		directorEntities[i] = directorEntity
+	}
+
+	return directorEntities, nil
+}
+
+func (c *DirectorController) Create(movie *entities.Director) error {
+	err := c.DirectorRepo.Create(movie)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *DirectorController) Delete(id interface{}) error {
+	err := c.DirectorRepo.Delete(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *DirectorController) Update(id int, movie *entities.Director) error {
+	err := c.DirectorRepo.Update(id, movie)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- repository/directorrepository.go
+++ repository/directorrepository.go
@@ -1,7 +1,6 @@
 package repository
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 
@@ -18,17 +17,17 @@ func NewDirectorRepository(db *sql.DB) *DirectorRepository {
 	return &DirectorRepository{db: db}
 }
 
-func (r *DirectorRepository) GetByID(ctx context.Context, id interface{}) (interface{}, error) {
+func (r *DirectorRepository) GetByID(id interface{}) (interface{}, error) {
 	var director Director
-	err := r.db.QueryRowContext(ctx, "SELECT * FROM directors WHERE id = $1", id).Scan(&director.ID, &director.Name, &director.BirthYear)
+	err := r.db.QueryRow("SELECT * FROM directors WHERE id = ?", id).Scan(&director.Name)
 	if err != nil {
 		return nil, fmt.Errorf("could not get director: %v", err)
 	}
 	return director, nil
 }
 
-func (r *DirectorRepository) GetAll(ctx context.Context) ([]interface{}, error) {
-	rows, err := r.db.QueryContext(ctx, "SELECT * FROM directors")
+func (r *DirectorRepository) GetAll() ([]interface{}, error) {
+	rows, err := r.db.Query("SELECT * FROM directors")
 	if err != nil {
 		return nil, fmt.Errorf("could not get directors: %v", err)
 	}
@@ -37,7 +36,7 @@ func (r *DirectorRepository) GetAll(ctx context.Context) ([]interface{}, error) 
 	var directors []interface{}
 	for rows.Next() {
 		var director Director
-		if err := rows.Scan(&director.ID, &director.Name, &director.BirthYear); err != nil {
+		if err := rows.Scan(&director.ID, &director.Name); err != nil {
 			return nil, fmt.Errorf("could not scan director: %v", err)
 		}
 		directors = append(directors, director)
@@ -45,34 +44,42 @@ func (r *DirectorRepository) GetAll(ctx context.Context) ([]interface{}, error) 
 	return directors, nil
 }
 
-func (r *DirectorRepository) Create(ctx context.Context, obj interface{}) (interface{}, error) {
-	director, ok := obj.(Director)
-	if !ok {
-		return nil, fmt.Errorf("invalid type, expected Director")
-	}
-	_, err := r.db.ExecContext(ctx, "INSERT INTO directors (name, birth_year) VALUES ($1, $2)", director.Name, director.BirthYear)
-	if err != nil {
-		return nil, fmt.Errorf("could not insert director: %v", err)
-	}
-	return director, nil
-}
-
-func (r *DirectorRepository) Update(ctx context.Context, id interface{}, obj interface{}) error {
+func (r *DirectorRepository) Create(obj interface{}) error {
 	director, ok := obj.(Director)
 	if !ok {
 		return fmt.Errorf("invalid type, expected Director")
 	}
-	_, err := r.db.ExecContext(ctx, "UPDATE directors SET name = $1, birth_year = $2 WHERE id = $3", director.Name, director.BirthYear, id)
+	_, err := r.db.Exec("INSERT INTO directors (name) VALUES ($1)", director.Name)
+	if err != nil {
+		return fmt.Errorf("could not insert director: %v", err)
+	}
+	return nil
+}
+
+func (r *DirectorRepository) Update(id interface{}, obj interface{}) error {
+	director, ok := obj.(Director)
+	if !ok {
+		return fmt.Errorf("invalid type, expected Director")
+	}
+	_, err := r.db.Exec("UPDATE directors SET name = $1 WHERE id = $2", director.Name, id)
 	if err != nil {
 		return fmt.Errorf("could not update director: %v", err)
 	}
 	return nil
 }
 
-func (r *DirectorRepository) Delete(ctx context.Context, id interface{}) error {
-	_, err := r.db.ExecContext(ctx, "DELETE FROM directors WHERE id = $1", id)
+func (r *DirectorRepository) Delete(id interface{}) error {
+	_, err := r.db.Exec("DELETE FROM directors WHERE id = $1", id)
 	if err != nil {
 		return fmt.Errorf("could not delete director: %v", err)
+	}
+	return nil
+}
+
+func (r *DirectorRepository) DeleteAll() error {
+	_, err := r.db.Exec("DELETE FROM directors")
+	if err != nil {
+		return fmt.Errorf("could not delete all directors: %v", err)
 	}
 	return nil
 }
